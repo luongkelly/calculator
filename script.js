2c7693b0a00e576694b878e1e9cfa698b0799ec2
Kelly Luong <kellyluong06@gmail.com>
2024-01-15 20:42:01 -0500

    Added functions and redid HTML

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-function add (a, b) {
-    return a + b;
+const numbers = document.querySelectorAll('.number');
+const operator = document.querySelectorAll('.operation');
+const allClear = document.getElementById('all-clear');
+const clear = document.getElementById('clear');
+const sign = document.getElementById('sign');
+const percent = document.getElementById('percent');
+const point = document.getElementById('point');
+const equals = document.getElementById('equals');
+const pscreen = document.getElementById('prev');
+const cscreen = document.getElementById('cuurent');
+
+let prevInput = '';
+let currentInput = '';
+
+function operate (operator, a, b) {
+    switch (operator) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '÷': return a / b;
+        default: null;
+    }
 }
 
-function subtract (a, b) {
-    return a - b;
-}
-
-function multiply (a, b) {
-    return a * b;
-}
-
-function divide (a, b) {
-    return a / b;
+function clear () {
+    currentInput = '';
+    prevInput = '';
 }
